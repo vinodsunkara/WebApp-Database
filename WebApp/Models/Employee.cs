aa0eca22a7dd17bcbcaea5475b7de91c3fd626c2
vinodsunkara <37222937+vinodsunkara@users.noreply.github.com>
2020-08-16 16:51:20 -0400

    Add project files.

---
+++ WebApp/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+    }
+}
